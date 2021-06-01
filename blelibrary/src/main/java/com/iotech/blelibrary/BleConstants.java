b27eceb6a872a75f343a7f0e5dca0850fde1b2c0
pawan <pawank@iotech.biz>
2021-06-01 11:32:32 +0530

    blev2

---
+++ blelibrary/src/main/java/com/iotech/blelibrary/BleConstants.java
@@ -0,0 +1,6 @@
+package com.iotech.blelibrary;
+
+public class BleConstants {
+    public static final String DiscoverService_iEDA = "0000CC01-0000-1000-8000-00805F9B34FB"
+    public static final String CUSTOM_SERVICE_iEDA = "0000CC02-0000-1000-8000-00805F9B34FB"
+}
--- blelibrary/src/main/java/com/iotech/blelibrary/Constants.kt
+++ blelibrary/src/main/java/com/iotech/blelibrary/Constants.kt
@@ -1,8 +1,6 @@
 package com.iotech.blelibrary
 
 class Constants {
-
-
     companion object {
         const val DiscoverService_iEDA = "0000CC01-0000-1000-8000-00805F9B34FB"
         const val CUSTOM_SERVICE_iEDA = "0000CC02-0000-1000-8000-00805F9B34FB"
